06b22a5d726d9b884bd28eaab8fe7f4fec11bcb5
TooKayAway <nhuff1@mail.csuchico.edu>
2018-03-29 21:39:39 -0700

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-//Author:
+//Author: Noah Huff
 #include <iostream>
 #include <fstream>
-
+#include <string>
 using namespace std;
 
 int main()
@@ -12,11 +12,17 @@ int main()
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
-
-
-
+  char a[80];
+  string line;
+  while(!in.eof()){
+    in>>a;
+    line = line + a;
+  }
+  first = line[0];
+  for ( int i = 0; i < line.length(); i++ ) {
+    last = line[i];
+  }
   //Then, finally, this displays the censored version:
   cout<<first<<"**"<<last<<endl;
-
   return 0;
 }
